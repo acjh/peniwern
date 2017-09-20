c5f93388554b3a9515887e7d1ed3f1d3a7d126a0
Aaron Chong <aaronjhchong@hotmail.com>
2017-09-20 23:25:44 +0800

    Add back-to-top-button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 (function () {
     $(function () {
+        // Configure behavior of anchors
         $("a[href='#']").click(function (e) {
             e.preventDefault();
+        });
+        $("a:not([href='#'])").click(function (e) {
+            $('html, body').animate({
+                scrollTop: $('[id="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+            }, 1000);
+            if (this === backToTopButton[0]) {
+                e.preventDefault();
+            }
+        });
+
+        // Configure visibility of #back-to-top-button
+        var backToTopButton = $('#back-to-top-button');
+        var previousScrollPosition = 0;
+        var buffer = 5/*px*/;
+        $(window).scroll(function () {
+            var currentScrollPosition = $(this).scrollTop();
+            if (currentScrollPosition === 0 || currentScrollPosition > previousScrollPosition) {
+                backToTopButton.fadeOut();
+            } else {
+                backToTopButton.fadeIn();
+            }
+            previousScrollPosition = currentScrollPosition;
         });
     });
 })();
